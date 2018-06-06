36ff1495820621d65c21f814fb4e35e335e598af
Krzysztof Miemiec <krzysztof.miemiec@gmail.com>
2018-06-06 18:19:48 +0200

    * Added missing test step * Corrected Circle PR parameters

--- src/config/ci/circle.ci.ts
+++ src/config/ci/circle.ci.ts
@@ -4,15 +4,35 @@ import { exec } from '../../utils/promises.util';
 import { Config } from '../config.interface';
 
 export module Circle {
+
+  const extractPullId = (pullPath: string) => undefinedIfFalse(pullPath)
+    ? pullPath.substring(pullPath.lastIndexOf('/') + 1)
+    : undefined;
+
+  const extractBaseBranch = async (repo: string, pull: string) => {
+    if (pull) {
+      // Workaround for missing Circle feature.
+      // See: https://discuss.circleci.com/t/how-to-get-the-pull-request-upstream-branch/5496
+      return (await exec(
+        `curl -fsSL https://api.github.com/repos/${repo}/pulls/${pull} | jq -r '.base.ref'`
+      )).stdout;
+    }
+    return undefined;
+  };
+
   export const config = async (): Promise<Partial<Config>> => {
     if (!process.env.CIRCLECI) {
       return {};
     }
-    const config = {
+    const pull = extractPullId(process.env.CIRCLE_PULL_REQUEST);
+    const repo = process.env.CIRCLE_PROJECT_USERNAME + '/' + process.env.CIRCLE_PROJECT_REPONAME;
+    const baseBranch = await extractBaseBranch(repo, pull);
+
+    return {
       git: {
-        baseBranch: process.env.CIRCLE_BRANCH,
-        repo: process.env.CIRCLE_PROJECT_USERNAME + '/' + process.env.CIRCLE_PROJECT_REPONAME,
-        pull: undefinedIfFalse(process.env.CIRCLE_PULL_REQUEST),
+        baseBranch,
+        repo,
+        pull,
         commit: process.env.CIRCLE_SHA1,
       },
       ci: {
@@ -23,13 +43,5 @@ export module Circle {
         buildDir: process.env.CIRCLE_WORKING_DIRECTORY,
       },
     };
-    if (config.git.pull) {
-      // Workaround for missing Circle feature.
-      // See: https://discuss.circleci.com/t/how-to-get-the-pull-request-upstream-branch/5496
-      config.git.pull = (await exec(
-        `curl -fsSL https://api.github.com/repos/${config.git.repo}/pulls/${config.git.pull} | jq -r '.base.ref'`
-      )).stdout;
-    }
-    return config;
   };
 }
